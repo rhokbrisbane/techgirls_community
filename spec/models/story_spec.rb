b34e0ec2f071d138f2973ef0648771155752e881
Gabriel Gizotti <hajime.gizotti@gmail.com>
2014-12-06 11:31:56 +1000

    fix minor syntax errors

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Story, type: :model do
   it { is_expected.to validate_presence_of(:body) }
 
-  it { is_expected.to belongs_to(:user) }
+  it { is_expected.to belong_to(:user) }
 end
