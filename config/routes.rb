66d563b04179c9ae6e0159c2d8cfddc4926040ad
Lucas Caton <lucascaton@gmail.com>
2014-12-06 13:37:05 +1000

    Fixing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
 
-  root "stories#index"
+  root 'stories#index'
 
   resources :stories, only: [:index, :create]
 end
