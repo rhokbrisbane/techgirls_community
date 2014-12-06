56a67c28dca00b13389daf6a016d8a22967f2fa5
Aline Caton <alinecaton@gmail.com>
2014-12-06 15:45:45 +1000

    Setting Mandrill up

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,9 +1,19 @@
 class StoriesController < ApplicationController
   def index
-    
   end
 
   def create
-    
+    # send_email(@story.author_name, @story.author_email)
+  end
+
+  def send_email(to_name, to_email)
+    ::EmailSender.send_email(
+      from_name:  'Tech Girls are Superheroes',
+      from_email: 'techgirlsaresuperheroes@gmail.com',
+      to_name:    to_name,
+      to_email:   to_email,
+      subject:    'Your story has been posted',
+      html:       render(layout: false,template: 'stories/email.html.haml').first
+    )
   end
 end
