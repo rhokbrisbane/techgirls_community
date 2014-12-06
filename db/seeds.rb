5e2fce049b6e77a29766f62fcbd1cb1290d3f54b
elliotthilaire <elliott.hilaire@gmail.com>
2014-12-07 09:44:18 +1000

    basic seed data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+case Rails.env
+when "development"
+   
+Story.create({
+  body: "My name is Jessica and I am a techgirl superstar.
+       My super power is writing LUA generators"
+	})
+
+Story.create({
+  body: "I am from Melbourne and my super power is protecticing the city from plus-one errors!"
+	})
+
+Story.create({
+	body: "I am 12 years old and I am a white hat hacker. Social justice through code!"
+	})
+
+Story.create({
+	body: "Yesterday I deployed my first ever app to heroku and shared the code on sourceforge!
+ I am officially now an open source developer!"
+	})
+
+end
+
