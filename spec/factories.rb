27ed6400b9549f6da075e56845d7e2e51ba33dcd
elliotthilaire <elliott.hilaire@gmail.com>
2014-12-07 13:29:26 +1000

    made story factory create a superhero

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,4 @@
 FactoryGirl.define do
-  factory :story do
-    body 'My story'
-  end
-
   factory :super_hero do
     name { Faker::Name.first_name }
     super_power { Faker::Lorem.word }
@@ -11,5 +7,10 @@ FactoryGirl.define do
     year_at_school '9'
     email { Faker::Internet.email }
     phone '90994242'
+  end
+
+  factory :story do
+    body 'My story'
+    super_hero { FactoryGirl.create(:super_hero) }
   end
 end
