19a376c5e2fd369a3ffcfbb4d3abdef893535fa0
Lucas Caton <lucascaton@gmail.com>
2014-12-07 15:11:57 +1000

    Fixing admin class

--- app/admin/super_hero.rb
+++ app/admin/super_hero.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Story do
+ActiveAdmin.register SuperHero do
   permit_params do
     [:body, :name, :super_power, :postcode, :age, :year_at_school, :email, :phone, :archetype]
   end
