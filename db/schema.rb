814f2ac47171833066eefba32ec17ed14f56895c
Lucas Caton <lucascaton@gmail.com>
2014-12-14 08:33:18 +1000

    Removing Comment model and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207023656) do
+ActiveRecord::Schema.define(version: 20141213222844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,15 +33,6 @@ ActiveRecord::Schema.define(version: 20141207023656) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "comments", force: true do |t|
-    t.integer  "story_id"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["story_id"], name: "index_comments_on_story_id", using: :btree
 
   create_table "stories", force: true do |t|
     t.text     "body"
